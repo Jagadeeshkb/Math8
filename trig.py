2276f34c44e0b092930cb5cee4b1bfe72e7f5970
Jagadeesh <50916738+Jagadeeshkb@users.noreply.github.com>
2021-04-24 17:40:37 +0530

    Create trig.py
    First Commit

---
+++ trig.py
@@ -0,0 +1,24 @@
+import math
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np
+#This Function will we give a plot with functions that are available in math library.
+
+
+#For list of available function use dir(math)
+
+def graph_math(start , end , by , func ):
+    dummy = str(func)
+    dum = dummy.split()[2][:-1]
+    lis = dir(math)
+    if dum in lis:
+        a = np.linspace(start,end,by)
+    
+        b= []
+        for i in a:
+            b.append(func(i))
+        plt.plot(a,b , label = str(dum))
+        plt.legend(loc='best')
+       
+    else:
+        raise NameError('Not Applicable function')
