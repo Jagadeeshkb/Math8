0fcf61d1d912688c1da3be6a0525164c4372fe8d
Jagadeesh <50916738+Jagadeeshkb@users.noreply.github.com>
2023-02-25 00:03:48 +0530

    Create fft.R

---
+++ fft.R
@@ -0,0 +1,9 @@
+# Generate a sine wave signal
+x <- seq(0, 2*pi, length.out = 100)
+y <- sin(5*x)
+
+# Compute the Fourier transform
+y_fft <- fft(y)
+
+# Plot the amplitude spectrum
+plot(Mod(y_fft), type = "l", xlab = "Frequency", ylab = "Amplitude")
